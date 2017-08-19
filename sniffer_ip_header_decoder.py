c4680e6e672d16fc3611e0ff2b732770d0f91686
#root <anchalpranav@gmail.com>
2017-08-19 17:14:28 +0530

    final complete

--- sniffer_ip_header_decoder.py
+++ sniffer_ip_header_decoder.py
@@ -32,3 +32,24 @@ class IP(Structure):
             self.protocol=self.protocol_map[self.protocol_num]
         except:
             self.protocol=str(self.protocol_num)
+    
+    if os.name == "nt":
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+    
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    try:
+        while True:
+            raw_buffer = sniffer.recvfrom(65565)[0]
+            ip_header = IP(raw_buffer[0:20])
+            print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+   
+    except KeyboardInterrupt:    
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)    
